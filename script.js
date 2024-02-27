4a52c2a2f1547559e93715965d37f8b9367b03fa
tube22-ex <79168960+tube22-ex@users.noreply.github.com>
2024-02-27 16:50:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const startBtn = document.getElementById('startBtn');
 startBtn.addEventListener('click',main)
 
 function main(){
-navigator.mediaDevices.getUserMedia({ video: { exact: "environment" }, audio: false })
+navigator.mediaDevices.getUserMedia({ video:  { facingMode: 'environment' },  audio: false })
   .then((stream) => {
     video.srcObject = stream;
   })
